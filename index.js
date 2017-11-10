73e8f7f6d4512c01f3ad0d757385d42963272ba1
Raja Sekar <rajasekar89be@gmail.com>
2017-11-10 22:45:11 +0530

    Revert "codemod to change babelcli -> @babel/core"
    This reverts commit 4e664732300599b68a9ce8b20f26269552a28a3f.

--- index.js
+++ index.js
@@ -1,4 +1,2 @@
 "use strict";
-const packageJsonCodemod = require('./lib/package-json-codemod');
-const testPath = "./test/package.json";
-packageJsonCodemod(testPath);
+module.exports = require("@babel/core");
